431169b4f3c59c484f825bb627763f90c7b5ee78
miraz-mondal <mirazmondal.official@gmail.com>
2024-05-09 00:37:18 +0530

    WIP

---
+++ script.js
@@ -0,0 +1,26 @@
+// JavaScript for FAQ section
+document.addEventListener("DOMContentLoaded", function () {
+  const questions = document.querySelectorAll(".question");
+
+  questions.forEach((question) => {
+    question.addEventListener("click", function () {
+      const answer = this.nextElementSibling;
+      answer.classList.toggle("show");
+    });
+  });
+});
+
+// JavaScript for dark mode toggle
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const mainContainer = document.querySelector(".main");
+
+darkModeToggle.addEventListener("click", () => {
+  mainContainer.classList.toggle("dark-mode");
+
+  // Toggle button text between "Light Mode" and "Dark Mode"
+  if (darkModeToggle.textContent === "Light Mode") {
+    darkModeToggle.textContent = "Dark Mode";
+  } else {
+    darkModeToggle.textContent = "Light Mode";
+  }
+});
